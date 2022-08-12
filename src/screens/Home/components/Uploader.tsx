7fa123693a2b2eaa7a03f0afa1fd7399185e3b7e
IDRISSHACKER <idrisscoder@gmail.com>
2022-08-12 20:32:09 +0100

    upload part2

--- src/screens/Home/components/Uploader.tsx
+++ src/screens/Home/components/Uploader.tsx
@@ -20,8 +20,8 @@ import { ResetTvOutlined, UploadFileOutlined } from "@mui/icons-material";
 
 function Uploader() {
 
-  const [dImg, setDImg] = React.useState(`${defaultImage}`);
-  const [img, setImg] = React.useState();
+  const dImg = `${defaultImage}`
+  const [img, setImg] = React.useState({});
   const [apercu, setApercu] = React.useState(dImg)
   const [uploading, setUploading] = React.useState(false)
   const [imgSelected, setImgSelected] = React.useState(false)
@@ -29,21 +29,55 @@ function Uploader() {
   const selectImage = React.useRef<HTMLInputElement>(null);
   const [imgName, setImgName] = React.useState("")
   const [imgSize, setImgSize] = React.useState(0)
+  const [size, setSize] = React.useState(`${imgSize}`)
 
   const handleSelectImg = ()=> {
     selectImage?.current?.click()
   }
 
-  const handleFile = (event:any) => {
-    const urlImg = URL.createObjectURL(event.currentTarget.files[0])
-    setImg(event?.currentTarget?.files[0])
-    setImgName(event?.currentTarget?.files[0]?.name)
-    setImgSize(event?.currentTarget?.files[0]?.size)
-    setApercu(urlImg)
-    setImgSelected(true)
-    console.log(event.currentTarget.files)
-    console.log(urlImg)
-  };
+  const handleFile = async(event:any) => {
+    if (event.currentTarget.files[0]){
+        const convertionNumber = 1000;
+        const acceptType = [
+          "image/png",
+          "image/jpg",
+          "image/jpeg",
+          "image/svg+xml",
+          "image/webp",
+        ];
+
+        if (acceptType.includes(event.currentTarget.files[0].type)){
+            const urlImg = URL.createObjectURL(event.currentTarget.files[0]);
+            setImg(event?.currentTarget?.files[0]);
+            setImgName(event?.currentTarget?.files[0]?.name);
+            setImgSize(event?.currentTarget?.files[0]?.size);
+            setApercu(urlImg);
+            setImgSelected(true);
+        }
+
+        const sizeKo = Math.floor(
+          event?.currentTarget?.files[0]?.size / convertionNumber
+        );
+        const sizeMo = Math.floor(sizeKo / convertionNumber)
+
+        if (sizeMo < 1) {
+          setSize(sizeKo + " KO");
+        } else {
+          setSize(sizeMo + " MO");
+        }
+        
+    }
+  }
+
+  const handleReset = (event:any) => {
+    setImg({})
+    setApercu(dImg)
+    setImgSelected(false)
+    setImgName("")
+    setImgSize(0)
+  }
+
+
 
   return (
     <div className="Upload-image">
@@ -70,7 +104,7 @@ function Uploader() {
                 </ListItem>
                 <Divider />
                 <ListItem button>
-                  <ListItemText className="imgSize" primary={`${imgSize}`} />
+                  <ListItemText className="imgSize" primary={`${size}`} />
                 </ListItem>
               </List>
             </CardContent>
@@ -82,6 +116,7 @@ function Uploader() {
                   color="error"
                   disableElevation
                   startIcon={<ResetTvOutlined />}
+                  onClick={handleReset}
                 >
                   Annuler
                 </Button>
